952e69e09444936c6d9c8375dfec03e9af8bfa79
ariana1almeida <arianaleu@hotmail.com>
2023-02-28 18:44:40 -0300

    Adicionando primeira resposta do desafio de código

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+/*
+ Observe o trecho de código abaixo:
+
+int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+{
+K = K + 1;
+SOMA = SOMA + K;
+}
+
+imprimir(SOMA);
+
+Ao final do processamento, qual será o valor da variável SOMA?
+*/
+public class Main {
+    public static void main(String[] args) {
+        final int INDICE=13;
+        int soma=0;
+        int k=0;
+
+        do {
+            k+=1;
+            soma+=k;
+        } while (k<INDICE);
+
+        System.out.println(soma);
+    }
+}
